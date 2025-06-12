0f5f5569870d1eabd3ef5ba7bcaf54a9b4ff4848
John Brewer <jbrewer@jera.com>
2025-06-12 14:36:50 -0700

    Initial Commit

---
+++ SharedDuck/SharedDuckApp.swift
@@ -0,0 +1,34 @@
+//
+//  SharedDuckApp.swift
+//  SharedDuck
+//
+//  Created by John Brewer on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SharedDuckApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
